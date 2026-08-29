6e459b243b2c65e8327aef1c4687767343bbb54c
linkunkun <linkunkun@secretlisa.com>
2026-08-29 19:18:24 +0800

    fix(mobile): fix responsive layout and CSS media queries compatibility for iOS/WeChat

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "ios14",
+    cssTarget: "ios14",
+  },
   server: {
     proxy: {
       "/api": {
